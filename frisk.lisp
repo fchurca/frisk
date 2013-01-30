2f8a9381a1296bb51c43276fba9f0c590c90924c
Federico Churca <federico.churca@gmail.com>
2013-01-30 19:29:52 -0300

    Dummy attacking function

--- frisk.lisp
+++ frisk.lisp
@@ -7,7 +7,9 @@
            :armies
            :player
            :read-map
-           :move-armies))
+           :move-armies
+           :place-armies
+           :attack))
 
 (in-package :ar.com.fchurca.frisk)
 
@@ -42,10 +44,11 @@
         (territories (make-hash-table))
         (filecontents (read file)))
     (dolist (territory (getf filecontents :territories))
-      (setf (gethash (car territory) territories)
-            (make-instance 'territory
-                           :name (second territory)
-                           :extra-armies (third territory))))
+      (destructuring-bind (key name extra-armies) territory
+        (setf (gethash key territories)
+              (make-instance 'territory
+                             :name name
+                             :extra-armies extra-armies))))
     territories))
 
 (defmethod read-map ((path pathname))
@@ -65,4 +68,24 @@
     (error "No hay suficientes ejércitos"))
   (decf (armies origin) amount)
   (incf (armies destination) amount))
+
+(defun place-armies (territory amount)
+  (unless (> amount 0)
+    (error "No se pueden poner 0 o menos ejércitos"))
+  (incf (armies territory) amount))
+
+(defun attack (origin destination)
+  (symbol-macrolet ((attackers (armies origin))
+                    (defenders (armies destination)))
+    (unless (> attackers 2)
+      (error "No se puede atacar teniendo 2 o menos ejércitos"))
+    (when (equal (owner destination) (owner origin))
+      (error "Un jugador no se puede atacar a si mismo"))
+    (let ((decrement (min defenders (- attackers 2))))
+      (decf attackers decrement)
+      (decf defenders decrement)
+      (when (= defenders 0)
+        (move-armies origin destination 1)
+        (setf (owner destination) (owner origin))))
+    (owner destination)))
 
