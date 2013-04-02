697cf329ca58fb42bcc2e6d31ba668cb76eaa49d
Federico Churca <federico.churca@gmail.com>
2013-04-02 15:23:06 -0300

    Polishing methods, messages. Attack now doesn't fail if defending with 1 unit.

--- frisk.lisp
+++ frisk.lisp
@@ -16,8 +16,8 @@
    (territories (error "Debe especificar los territorios") ir)
    (frontiers (error "Debe especificar las fronteras") ir)))
 
-(defgeneric read-map (file)
-  (:method ((file stream))
+(defgeneric read-map (file &key)
+  (:method ((file stream) &key)
    (let ((*read-eval* nil)
          (territories (make-hash-table))
          (frontiers (make-container 'graph-container))
@@ -33,10 +33,10 @@
            (add-edge-between-vertexes frontiers key frontierkey))))
      (make-instance 'game :territories territories :frontiers frontiers))) 
 
-  (:method ((path pathname))
+  (:method ((path pathname) &key)
    (with-open-file (file path) (read-map file))) 
 
-  (:method ((path string))
+  (:method ((path string) &key)
    (read-map
      (parse-namestring
        (if (probe-file path)
@@ -55,7 +55,9 @@
       (find-vertex graph origin-key)
       (find-vertex graph destination-key))))
 
-(defun move-armies (game origin-key destination-key amount)
+(defgeneric move-armies (game from to amount &key))
+
+(defmethod move-armies (game origin-key destination-key amount &key)
   (let ((origin (territory game origin-key))
         (destination (territory game destination-key)))
     (unless (territories-connected-p game origin-key destination-key)
@@ -71,7 +73,9 @@
     (decf (armies origin) amount) 
     (incf (armies destination) amount)))
 
-(defun place-armies (game territory-key amount)
+(defgeneric place-armies (game where amount &key))
+
+(defmethod place-armies (game territory-key amount &key)
   (let ((territory (territory game territory-key)))
     (unless (> amount 0)
       (error "No se pueden poner 0 o menos ejércitos"))
@@ -79,7 +83,9 @@
       (error "El jugador de turno no controla ese territorio")) 
     (incf (armies territory) amount)))
 
-(defun attack (game origin-key destination-key)
+(defgeneric attack (game from to &key))
+
+(defmethod attack (game origin-key destination-key &key)
   (symbol-macrolet
     ((origin (territory game origin-key))
      (destination (territory game destination-key))
@@ -90,14 +96,14 @@
     (unless (> attackers 2)
       (error "No se puede atacar teniendo 2 o menos ejércitos"))
     (unless (eq (owner origin) (turn-player game))
-      (error "El jugador de turno no controla esos territorios"))
+      (error "El jugador de turno no controla ese territorio"))
     (when (equal (owner destination) (owner origin))
       (error "Un jugador no se puede atacar a sí mismo"))
-    (let ((decrement (min defenders (- attackers 2))))
+    (let ((decrement (1+ (min defenders (- attackers 2)))))
       (decf attackers (random decrement))
       (decf defenders (random decrement))
       (when (= defenders 0)
         (setf (owner destination) (owner origin))
         (move-armies game origin-key destination-key 1)))
-    (owner destination)))
+    (eq (owner origin) (owner destination))))
 
