ab253d393cbd014b6f4c4a9c0f5b3067fa308c3d
Federico Churca-Torrusio <federico.churca@gmail.com>
2013-12-09 21:12:02 -0300

    More fixing of exported symbols was realised

--- package.lisp
+++ package.lisp
@@ -5,22 +5,30 @@
         :metabang-bind)
   (:import-from :metatilities :defclass*)
   (:import-from :alexandria :shuffle :hash-table-keys)
-  (:export :territory
-           :name
-           :extra-armies
-           :owner
-           :armies
-           :player
-           :game
-           :read-map
-           :read-game
-           :territory-keys
-           :player-keys
-           :territories-connected-p
-           :add-player
-           :shuffle-territories
-           :move-armies
-           :place-armies
-           :attack
-           :print-game))
+  (:export
+    ; Classes
+    :territory
+    :game-map
+    :player
+    :game
+    ; Accessors
+    :name
+    :extra-armies
+    :owner
+    :armies
+    :territories
+    :frontiers
+    :players
+    ; Functions and methods and such, oh my!
+    :read-map
+    :read-game
+    :territory-keys ; These two are
+    :player-keys    ; kinda-accessor-ish 
+    :territories-connected-p
+    :add-player
+    :shuffle-territories
+    :move-armies
+    :place-armies
+    :attack
+    :print-game))
 
